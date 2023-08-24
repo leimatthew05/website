c7c37a99b65f687b42960a005041a37ffd929b8c
leimatthew05 <64387516+leimatthew05@users.noreply.github.com>
2023-08-24 13:49:31 -0700

    trying to deploy pt 2

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -12,70 +12,70 @@ import '../css/style.css'
 
 
 
-class ArtGallery extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            images: this.props.images,
-            index: -1
-        };
+// class ArtGallery extends React.Component {
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             images: this.props.images,
+//             index: -1
+//         };
 
-        this.setIndex.bind(this)
-    }
+//         this.setIndex.bind(this)
+//     }
 
-    setIndex(index){
-        this.setState({index: index});
-    }
+//     setIndex(index){
+//         this.setState({index: index});
+//     }
 
-    render(){
-        var images = this.props.images
+//     render(){
+//         var images = this.props.images
 
-        var gallery = images.map((image, index) => {
-            return (
-            <Col className="gallerycell" onClick={() => this.setIndex(index)} md={image.w} 
-                style={{backgroundImage: `url(${image.src})`}}>
-            </Col>
-            )
-        })
+//         var gallery = images.map((image, index) => {
+//             return (
+//             <Col className="gallerycell" onClick={() => this.setIndex(index)} md={image.w} 
+//                 style={{backgroundImage: `url(${image.src})`}}>
+//             </Col>
+//             )
+//         })
     
-        console.log(gallery)
+//         console.log(gallery)
         
-        const index = this.state.index
+//         const index = this.state.index
     
-        const currentImage = images[index];
-        const nextIndex = (index + 1) % images.length;
-        const nextImage = images[nextIndex] || currentImage;
-        const prevIndex = (index + images.length - 1) % images.length;
-        const prevImage = images[prevIndex] || currentImage;
+//         const currentImage = images[index];
+//         const nextIndex = (index + 1) % images.length;
+//         const nextImage = images[nextIndex] || currentImage;
+//         const prevIndex = (index + images.length - 1) % images.length;
+//         const prevImage = images[prevIndex] || currentImage;
     
-        const handleClick = (index, item) => this.setIndex(index);
-        const handleClose = () => this.setIndex(-1);
-        const handleMovePrev = () => this.setIndex(prevIndex);
-        const handleMoveNext = () => this.setIndex(nextIndex);
+//         const handleClick = (index, item) => this.setIndex(index);
+//         const handleClose = () => this.setIndex(-1);
+//         const handleMovePrev = () => this.setIndex(prevIndex);
+//         const handleMoveNext = () => this.setIndex(nextIndex);
     
-        return (
-            <div>
-                <Row style={{width: "100vw !important"}}>
-                {gallery}
-                </Row>
-                {!!currentImage && (
-                /* @ts-ignore */
-                <Lightbox
-                    mainSrc={currentImage.src}
-                    prevSrc={prevImage.src}
-                    nextSrc={nextImage.src}
-                    imageTitle={currentImage.caption}
-                    mainSrcThumbnail={currentImage.src}
-                    nextSrcThumbnail={nextImage.src}
-                    prevSrcThumbnail={prevImage.src}
-                    onCloseRequest={handleClose}
-                    onMovePrevRequest={handleMovePrev}
-                    onMoveNextRequest={handleMoveNext}
-                />
-                )}
-            </div>
-        )
-    }
-}
+//         return (
+//             <div>
+//                 <Row style={{width: "100vw !important"}}>
+//                 {gallery}
+//                 </Row>
+//                 {!!currentImage && (
+//                 /* @ts-ignore */
+//                 <Lightbox
+//                     mainSrc={currentImage.src}
+//                     prevSrc={prevImage.src}
+//                     nextSrc={nextImage.src}
+//                     imageTitle={currentImage.caption}
+//                     mainSrcThumbnail={currentImage.src}
+//                     nextSrcThumbnail={nextImage.src}
+//                     prevSrcThumbnail={prevImage.src}
+//                     onCloseRequest={handleClose}
+//                     onMovePrevRequest={handleMovePrev}
+//                     onMoveNextRequest={handleMoveNext}
+//                 />
+//                 )}
+//             </div>
+//         )
+//     }
+// }
 
-export default ArtGallery;
+// export default ArtGallery;
