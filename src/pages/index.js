7369ff5a877539637b404d5a9bb95c3ba532f59e
leimatthew05 <64387516+leimatthew05@users.noreply.github.com>
2024-11-12 08:58:23 -0800

    Updates 11/12/2024
    new profile picture, and some minor changes to my portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,15 +30,6 @@ const Page = () => {
             data-ratio="1.78343949044586">
           </iframe>
           <iframe
-            class="speakerdeck-iframe"
-            frameborder="0"
-            src="https://speakerdeck.com/player/65d53621ec694c25ac15c66efa9fecff"
-            title="Heathers (&quot;Beautiful&quot; Part One)"
-            allowfullscreen="true"
-            style={{border: "0px", background: "padding-box padding-box rgba(0, 0, 0, 0.1)", margin: "0 20% auto", padding: "0px", borderradius: "6px", boxshadow: "rgba(0, 0, 0, 0.2) 0px 5px 40px", width: "60%", height: "600px", aspectratio: "560 / 314;"}}
-            data-ratio="1.78343949044586">
-          </iframe>
-          <iframe
             style={{width: "60%", height: "600px", margin: "0 20% auto"}}
             src="https://www.youtube.com/embed/GyjHQcxAK9A?si=EdZ339pb3IJ48Ipk"
             title="YouTube video player"
@@ -47,6 +38,16 @@ const Page = () => {
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen>
           </iframe>
+          <iframe
+            class="speakerdeck-iframe"
+            frameborder="0"
+            src="https://speakerdeck.com/player/65d53621ec694c25ac15c66efa9fecff"
+            title="Heathers (&quot;Beautiful&quot; Part One)"
+            allowfullscreen="true"
+            style={{border: "0px", background: "padding-box padding-box rgba(0, 0, 0, 0.1)", margin: "0 20% auto", padding: "0px", borderradius: "6px", boxshadow: "rgba(0, 0, 0, 0.2) 0px 5px 40px", width: "60%", height: "600px", aspectratio: "560 / 314;"}}
+            data-ratio="1.78343949044586">
+          </iframe>
+
           <iframe
             class="speakerdeck-iframe"
             frameborder="0"
@@ -79,15 +80,7 @@ const Page = () => {
             style={{border: "0px", background: "padding-box padding-box rgba(0, 0, 0, 0.1)", margin: "0", padding: "0px", borderradius: "6px", boxshadow: "rgba(0, 0, 0, 0.2) 0px 5px 40px", width: "100%", height: "300px", aspectratio: "560 / 314;"}}
             data-ratio="1.78343949044586">
           </iframe>
-          <iframe
-            class="speakerdeck-iframe"
-            frameborder="0"
-            src="https://speakerdeck.com/player/65d53621ec694c25ac15c66efa9fecff"
-            title="Heathers (&quot;Beautiful&quot; Part One)"
-            allowfullscreen="true"
-            style={{border: "0px", background: "padding-box padding-box rgba(0, 0, 0, 0.1)", margin: "0", padding: "0px", borderradius: "6px", boxshadow: "rgba(0, 0, 0, 0.2) 0px 5px 40px", width: "100%", height: "300px", aspectratio: "560 / 314;"}}
-            data-ratio="1.78343949044586">
-          </iframe>
+
           <iframe
             style={{width: "100%", height: "300px", margin: "0"}}
             src="https://www.youtube.com/embed/GyjHQcxAK9A?si=EdZ339pb3IJ48Ipk"
@@ -96,6 +89,15 @@ const Page = () => {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen>
+          </iframe>
+          <iframe
+            class="speakerdeck-iframe"
+            frameborder="0"
+            src="https://speakerdeck.com/player/65d53621ec694c25ac15c66efa9fecff"
+            title="Heathers (&quot;Beautiful&quot; Part One)"
+            allowfullscreen="true"
+            style={{border: "0px", background: "padding-box padding-box rgba(0, 0, 0, 0.1)", margin: "0", padding: "0px", borderradius: "6px", boxshadow: "rgba(0, 0, 0, 0.2) 0px 5px 40px", width: "100%", height: "300px", aspectratio: "560 / 314;"}}
+            data-ratio="1.78343949044586">
           </iframe>
           <iframe
             class="speakerdeck-iframe"
